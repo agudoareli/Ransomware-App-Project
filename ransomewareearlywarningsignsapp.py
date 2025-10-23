aac2db8245f53a311497c41638e4724f932235eb
agudoareli <160306633+agudoareli@users.noreply.github.com>
2025-10-23 14:35:44 -0600

    Update ransomewareearlywarningsignsapp.py

--- ransomewareearlywarningsignsapp.py
+++ ransomewareearlywarningsignsapp.py
@@ -6,19 +6,22 @@ from pytrends.request import TrendReq # allows us to get data from google trends
 import matplotlib.pyplot as plt # used to make charts and save them as images
 import pandas as pd # helps us work with data tables
 from datetime import datetime # helps us work with date and time
+import time 
 
 class TrendsAPI:
+  def __init__(self):
+    self.request = TrendReq(hl='en-US', tz=360)
+
   def _fetch_google_trends(self, keyword): # define a function to fetch the google trends using a keyword
-    request = self.TrendReq(hl='en-US', tz=360).build_payload([keyword], cat=0, timeframe='today 12-m', geo='', gprop='')
-    self.time.sleep(1.5)
+    self.request.build_payload([keyword], cat=0, timeframe='today 12-m', geo='', gprop='')
+    time.sleep(2)
     # [keyword]: a list of the search term
     # cat=0: no specific category
     # timeframe='today 12-m': search from the last 12 months of data
     # geo='': no specific country
     # gprop='': no search filter, search from any info source
-    
     data = self.request.interest_over_time() # downloads the trend data as a table, rows = days/weeks, cols = the frequency of search for the keyword
-    self.time.sleep(1.5)
+    time.sleep(2)
     if not data:
       return "No data available for this keyword."
     # Plotting the data
@@ -35,10 +38,11 @@ class TrendsAPI:
     return f"Trend data for '{keyword}' shown below.", plot_filename  # show the user a message and the image file that was saved
 
   def fetch_google_trends_safely(self, keyword):
-    try:
-      result = self._fetch_google_trends(keyword)
-      return result 
-    except:
-      return "Too many requests, please wait 15 seconds."
+    return self._fetch_google_trends(keyword)
+    # try:
+    #   result = self._fetch_google_trends(keyword)
+    #   return result 
+    # except:
+    #   return "Too many calls, please wait 15 seconds and try again."
 
 
